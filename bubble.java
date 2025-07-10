1aa8c2fa199a215d169c0a531e59ad9e1d48cd4c
ABHISHEK KUMAR SHARMA <151908859+Abhishekkrsharma07@users.noreply.github.com>
2025-07-10 22:26:46 +0530

    Add files via upload

--- bubble.java
+++ bubble.java
@@ -1,32 +1,25 @@
-class bubble {
-    public static void PrintArry(int arr[]) {
-
+import java.util.*;
+class bubble{
+    public static void bubblesort(int arr[], int n){
         
-        for(int i=0;i<arr.length;i++){
-        
-        System.out.println(arr[i]+" ");
-    }
-    System.out.println();
-}
-
-public static void main(String []args){
-
-    int i,j;
-    
-    int arr[]={1,3,4,7,8,7};
-    //bubble sort
-    
-    for(i=0;i<arr.length-i;i++){
-            for(j=0;j<arr.length-i-1;j++){
-    
-                if(arr[j]>arr[j+1]){
-                    //Now we can swap with the old method which we have learn in basics;
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+        for(int i=n-1; i>=1; i--){
+            
+            for(int j=0; j<=i-1; j++){
+                if(arr[j+1] > arr[j]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
-        PrintArry(arr);
-}
+       
+    }
+    
+    public static void main(String[] args){
+        int arr[]={13,46,24,52,20,9};
+        int n=arr.length;
+        bubblesort(arr, n);
+        System.out.println(Arrays.toString(arr));
+
+    }
 }
