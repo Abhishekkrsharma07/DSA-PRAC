d60ac9de15a9e334d07d356bc4239ed1201a1502
ABHISHEK KUMAR SHARMA <151908859+Abhishekkrsharma07@users.noreply.github.com>
2025-07-25 23:22:57 +0530

    Add files via upload

---
+++ permutation.java
@@ -0,0 +1,36 @@
+class permutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        // finding the number which decreses first
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
