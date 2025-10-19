103212bf53dd798f3e20fd47d7e47df2caed524b
ABHISHEK KUMAR SHARMA <151908859+Abhishekkrsharma07@users.noreply.github.com>
2025-10-19 07:59:39 +0530

    Add files via upload

---
+++ summary226.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class summary226 {
+
+    public static List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        if (nums.length == 0) return result;
+
+        int start = nums[0]; 
+
+        for (int i = 1; i <= nums.length; i++) {
+            
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+
+                
+                if (i < nums.length) start = nums[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 6, 7, 9};
+        System.out.println(summaryRanges(nums));
+        
+    }
+}
