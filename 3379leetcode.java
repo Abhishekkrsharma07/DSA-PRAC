36b029e9c9fd8ac129d9ff2ac2eac9e46a49704e
ABHISHEK KUMAR SHARMA <151908859+Abhishekkrsharma07@users.noreply.github.com>
2026-02-05 23:23:39 +0530

    Create 3379leetcode.java

---
+++ 3379leetcode.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                ans[i]=0;
+            }
+            else{
+                int newIndex = ((i+nums[i])%n+n)%n;
+                ans[i] = nums[newIndex];
+            }
+        } 
+        return ans; 
+        }
+}
